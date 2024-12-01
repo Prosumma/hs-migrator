9dc5b5b1583941ca6c1b5f751302bdbefe6046c4
Gregory Higley <greg@prosumma.com>
2024-12-01 14:34:39 -0500

    Basic commands

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,25 @@
 module Main (main) where
 
-import Lib
+import Formatting
+import Options.Applicative
+import Prosumma.Util
 import RIO
 
+data Command = Migrate | New FilePath deriving (Eq, Show)
+
+parseCommand :: Parser Command
+parseCommand = subparser (migrate <> new)
+  where
+    migrate = command "migrate" (info (pure Migrate) (progDesc ""))
+    new = command "new" (info (New <$> argument str (metavar "DESCRIPTION")) (progDesc ""))
+
 main :: IO ()
-main = someFunc
+main = do 
+  options <- logOptionsHandle stderr True
+  withLogFunc options $ \lf -> runRIO lf $ do
+    cmd <- liftIO $ execParser opts
+    case cmd of
+      Migrate -> logDebug "Migrate!"
+      New fp -> logDebug $ uformat ("New: " % string) fp 
+  where
+    opts = info (parseCommand <**> helper) ( fullDesc <> progDesc "PostgreSQL migrations CLI tool" <> header "pg-migrations - a PostgreSQL migration manager" )
